8a492eeaca6b70526be04859c6147d7d2e6ebe7c
nk <nk@localhost.localdomain>
2022-09-30 12:57:26 +0200

    modified index.php

--- index.php
+++ index.php
@@ -9,8 +9,6 @@
 		<p><b>An Example of PHP in Action</b></p>
 		<?php echo "The Current Date and Time is: <br />"; 
 		echo date("g:i A l, F j Y.");?> </p>
-	<h2>PHP Information</h2> 
-		<p> <?php phpinfo(); ?> </p> 
-        <h2>GOING ON VACATION</h2>
+        <h2>GOING TO THE MOON</h2>
 	</body> 
 </html>
